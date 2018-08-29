b73a5e9a9169496b90ae10e5b1904cf942248fe7
fdolsky321 <fdolsky@gmail.com>
2018-08-29 10:55:08 +0200

    Update SetupParameters.cs

--- speech_recognition/SetupParameters.cs
+++ speech_recognition/SetupParameters.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
@@ -10,9 +10,22 @@ namespace SpeechRecognition
 {
     public class SetupParameters : CodeActivity
     {
+        public enum Engineenum
+        {
+            Google,
+            GoogleCloud,
+            Bing,
+            Houndify,
+            IBM,
+            Sphinx,
+            Wit
+        }
         // ID of the Input Device - Microphone ID - Let it be empty to use the default one
         [Category("Input")]
         public InArgument<Int32> DeviceID {get; set;}
+
+        [Category("Input")]
+        public InArgument<string> FilePath { get; set; }
 
         [Category("Logon")]
         public InArgument<string> APIKey { get; set; }
@@ -24,23 +37,26 @@ namespace SpeechRecognition
         public InArgument<string> Password { get; set; }
 
         [Category("Options")]
-        public InArgument<string> Engine { get; set; }
+        public Engineenum Engine { get; set; }
 
         // True by Default, False if you want to set the Threshold Value
+        private InArgument<Boolean> threshold = true;
         [Category("Options")]
-        public InArgument<Boolean> DynamicEnergyThreshold { get; set; }
+        public InArgument<Boolean> DynamicEnergyThreshold { get { return this.threshold; } set { this.threshold = value; } }
 
         // Set up in case you are not using dynamic energy threshold
         // To eliminate Ambient noise, typical values in range (150,3500), silent room -> low number
+        private InArgument<Int32> energyThreshold = 300;
         [Category("Options")]
-        public InArgument<Int32> EnergyThresholdValue { get; set; }
+        public InArgument<Int32> EnergyThresholdValue { get {return this.energyThreshold; } set {this.energyThreshold = value; } }
 
         // Set up in case you are not using dynamic energy threshold
         // Pause between words, as you reach the threshold, recording will stop (seconds)
+        private InArgument<double> pauseThresholdValue = 0.8;
         [Category("Options")]
-        public InArgument<double> PauseThreshold { get; set; }
-        
-        // Throw an error, if there is no speech until this timeout (seconds)
+        public InArgument<double> PauseThreshold { get {return this.pauseThresholdValue; } set {this.pauseThresholdValue = value; } }
+
+        // Throw an error, if there is no speech until this timeout (seconds) 
         [Category("Options")]
         public InArgument<double> TimeoutSeconds { get; set; }
         
@@ -48,8 +64,9 @@ namespace SpeechRecognition
         [Category("Options")]
         public InArgument<double> PhraseTimeLimit { get; set; }
 
+        private InArgument<string> inputLanguageValue = "en-US";
         [Category("Options")]
-        public InArgument<string> InputLanguage { get; set; }
+        public InArgument<string> InputLanguage { get {return this.inputLanguageValue; } set {this.inputLanguageValue = value; } }
 
         [Category("Output")]
         public OutArgument<IEnumerable<object>> OutputParameters { get; set; }
@@ -59,15 +76,22 @@ namespace SpeechRecognition
             var deviceID = (int) DeviceID.Get(context);
             var dynamicEnergyThreshold = (Boolean) DynamicEnergyThreshold.Get(context);
             var energyThresholdValue = (Int32) EnergyThresholdValue.Get(context);
+            if (energyThresholdValue <= 0)
+                energyThresholdValue = 300;
             var pauseThreshold = PauseThreshold.Get(context);
-            var timeoutSeconds = TimeoutSeconds.Get(context);
-            var phraseTimeLimit = PhraseTimeLimit.Get(context);
+            if (pauseThreshold <= 0)
+                pauseThreshold = 0.8;
+            double? timeoutSeconds = TimeoutSeconds.Get(context);
+            double? phraseTimeLimit = PhraseTimeLimit.Get(context);
             var inputLanguage = InputLanguage.Get(context);
+            if (inputLanguage.Length < 2)
+                inputLanguage = "en-US";
             var apiKey = APIKey.Get(context);
             var username = Username.Get(context);
             var password = Password.Get(context);
-            var engine = Engine.Get(context);
-            IEnumerable<object> inputParameters = new object []{ deviceID, inputLanguage, apiKey, username, password, engine, dynamicEnergyThreshold, energyThresholdValue, pauseThreshold, timeoutSeconds, phraseTimeLimit};
+            var engine = Engine;
+            var filePath = FilePath.Get(context);
+            IEnumerable<object> inputParameters = new object []{ deviceID, filePath, inputLanguage, apiKey, username, password, engine, dynamicEnergyThreshold, energyThresholdValue, pauseThreshold, timeoutSeconds, phraseTimeLimit};
             OutputParameters.Set(context, inputParameters);
         }
     }
